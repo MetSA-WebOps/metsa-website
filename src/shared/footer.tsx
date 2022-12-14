4abb93df05f05037f01328672a600637981b5e2b
Rasagnya <gudipudirasagnya@gmail.com>
2022-12-15 01:52:36 +0530

    initial footer

--- src/shared/footer.tsx
+++ src/shared/footer.tsx
@@ -1,5 +1,23 @@
+import content from '../content/footer.json';
+
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <img src="src/assets/logo_2.png" alt="metsa" style={{ marginLeft: '40px' }} />
+      <div >
+        <p> Contact Us </p>
+        <p> {content.name} </p>
+        <p> {content.position}</p>
+        <p> {content.organization}</p>
+        <p>{content.address}</p>
+        <p> {content.mail_1}</p>
+        <p>{content.phone}</p>
+        <p>{content.mail_2}</p>
+      </div>
+      <img src="src/assets/iitm_logo.png" alt="iitm" style={{ marginRight: '40px' }} />
+    </div>
+  );
 }
 
 export default Footer;
