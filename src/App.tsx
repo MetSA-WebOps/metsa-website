5e58ad6172f9857c9cdd6fadb1f14351e9bd2923
Rasagnya <gudipudirasagnya@gmail.com>
2022-12-14 23:07:59 +0530

    header

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,6 @@ function App() {
 
   return (
     <div className="App">
-
       <RouterProvider router={router} />
       <Footer />
     </div>
--- src/shared/header.tsx
+++ src/shared/header.tsx
@@ -1,13 +1,17 @@
 import { NavLink } from "react-router-dom";
-import { Link, Outlet } from "react-router-dom";
+import './style.css';
 
 function Header() {
   return (
     <nav className="header">
-      <NavLink to="/"><img src="../assets/logo.png" /></NavLink>
+      <NavLink to="/" style={{ display: 'flex' }}><img src="src/assets/logo.png" className="logo" /></NavLink>
       <div className="nav-links">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/event/1">Event</NavLink>
+        <NavLink to="/" className="link">Home</NavLink>
+        <NavLink to="/about" className="link">About Us</NavLink>
+        <NavLink to="/event/1" className="link">Events</NavLink>
+        <NavLink to="/sponsors" className="link">Sponsors</NavLink>
+        <NavLink to="/registration" className="link">Registration</NavLink>
+        <NavLink to="/gallery" className="link">Gallery</NavLink>
       </div>
     </nav >
   );
